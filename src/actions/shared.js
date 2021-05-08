521f670c619083ca290624c2f589c467f15dc294
AhmedGafar <ahmed.gafar@pixelogicmedia.com>
2021-05-08 11:25:49 +0200

    added initialize actions and api calls

---
+++ src/actions/shared.js
@@ -0,0 +1,19 @@
+import { getInitialData } from "../utils/api";
+import { setAuthedUser } from "../actions/authedUser";
+import { receiveUsers } from "../actions/users";
+import { receiveQuestions } from "../actions/questions";
+import { showLoading, hideLoading } from "react-redux-loading";
+
+const AUTHED_ID = "tylermcginnis";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    dispatch(showLoading());
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(hideLoading());
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
