93a1f30c4a9449043032a8aaf3bce20f76301e85
AhmedGafar <ahmed.gafar@pixelogicmedia.com>
2021-05-13 13:49:33 +0200

    added login funtionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
 import "../index.css";
 import { connect } from "react-redux";
 import React, { Component, useEffect } from "react";
+import { setAuthedUser } from "../actions/authedUser";
 
 function User(props) {
+  const { users, dispatch } = props;
   // useEffect(() => {
   //   dispatch(handleInitialData());
   // }, [dispatch]);
@@ -10,22 +12,39 @@ function User(props) {
   // const handleClick = () => {
   //   dispatch(saveQuestion("optionOneText", "optionTwoText", "tylermcginnis"));
   // };
+  const changefunction = (event) => {
+    dispatch(
+      setAuthedUser(
+        users.filter((user) => user.id === event.target.value)[0].id
+      )
+    );
+  };
+
   return (
     <div>
+      <select value={null} onChange={changefunction}>
+        {users
+          ? props.users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))
+          : "loading    "}
+      </select>
     </div>
   );
 }
 
-function mapStateToProps({authedUser , users }) {
-    if (JSON.stringify(users) !== "{}")
-      return {
-        users: Object.values(users).sort(
-          (b, a) => 
-            (a.questions.length +
-              Object.keys(a.answers).length) -
-            (b.questions.length + Object.keys(b.answers).length)
-        ),
-      };
-  }
+function mapStateToProps({ authedUser, users }) {
+  if (JSON.stringify(users) !== "{}")
+    return {
+      users: Object.values(users).sort(
+        (b, a) =>
+          a.questions.length +
+          Object.keys(a.answers).length -
+          (b.questions.length + Object.keys(b.answers).length)
+      ),
+    };
+}
 
 export default connect(mapStateToProps)(User);
